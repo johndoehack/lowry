c5ad9413a4fb0a9a088df25df32686b4152ddf84
Jonathan Freedman <jonafree@gmail.com>
2012-10-08 23:25:47 -0700

    first post

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+
+gem 'sinatra-config-file', '1.0'
+gem 'whois'
+gem 'json'
---
+++ server.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'whois'
+require 'json'
+
+def finish_whois(host, w)
+  resp = {
+    "hostname"=>host,
+    "registered"=>w.registered?,
+    "available"=>w.available?
+  }
+  if w.registered?
+    resp["created_on"] = w.created_on
+    resp["updated_on"] = w.updated_on
+    resp["expires_on"] = w.expires_on
+    resp["status"] = w.status
+    resp["referral_url"] = w.referral_url
+    resp["referral_whois"] = w.referral_whois
+    resp["disclaimer"] = w.disclaimer
+  end
+
+  status 200
+  headers "Content-Type" => "application/json"
+  body JSON.dump(resp)
+end
+
+get '/whois/:host' do
+  host = "#{params[:host]}"
+  begin
+    w = Whois.whois(host)
+    finish_whois(host, w)
+  rescue Whois::ServerNotFound => e
+    status 503
+  end
+  
+end
+
+get '/*' do
+  halt 404
+end
